a876e87263a9b272cf550ae819f33580709f0058
ManUnited1878 <ktf3@pdx.edu>
2019-05-02 17:15:20 -0700

    Added code for both forward and reverse

--- Control.py
+++ Control.py
@@ -1,19 +1,66 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+# Import the PCA9685 module.
+import Adafruit_PCA9685
+
+pwm = Adafruit_PCA9685.PCA9685()
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-@socketio.on('myevent')
-def handle_message(message):
-    print('received message: ' + message)
+#@socketio.on('myevent')
+#def handle_message(message):
+#    print('received message: ' + message)
 
 @socketio.on('arrows')
 def handle_message(message):
     print('received message: ' + str(message))
+    if message==1:
+        servo_min = 150  # Min pulse length out of 4096
+        servo_max = 600  # Max pulse length out of 4096
 
+
+      # Set frequency to 60hz, good for servos.
+        pwm.set_pwm_freq(60)
+
+        print('Moving servo on channels 0-3, press Ctrl-C to quit...')
+        while True:
+          # Move servo on channel O between extremes.
+          #360=clockwise, 0=counterclockwise
+          pwm.set_pwm(0, 0, servo_max) #Left Front Drive 
+          time.sleep(1)
+          pwm.set_pwm(1, 360, servo_max) #Right Front Drive
+          time.sleep(1)
+          pwm.set_pwm(2, 0, servo_max) #Left Rear Drive 
+          time.sleep(1)
+          pwm.set_pwm(3, 360, servo_max) #Right Rear Drive
+          time.sleep(1)
+   
+    if message==3:
+        servo_min = 150  # Min pulse length out of 4096
+        servo_max = 600  # Max pulse length out of 4096
+
+
+      # Set frequency to 60hz, good for servos.
+        pwm.set_pwm_freq(60)
+
+        print('Moving servo on channels 0-3, press Ctrl-C to quit...')
+        while True:
+          # Move servo on channel O between extremes.
+          #360=clockwise, 0=counterclockwise
+          pwm.set_pwm(0, 360, servo_max) #Left Front Drive 
+          time.sleep(1)
+          pwm.set_pwm(1, 0, servo_max) #Right Front Drive
+          time.sleep(1)
+          pwm.set_pwm(2, 360, servo_max) #Left Rear Drive 
+          time.sleep(1)
+          pwm.set_pwm(3, 0, servo_max) #Right Rear Drive
+          time.sleep(1)
+  
+    return render_template('index.html')
 @app.route("/")
 def root():
   message = "Hey World"
--- ESCcontrol.py
+++ ESCcontrol.py
@@ -1,7 +1,5 @@
 # Simple demo of of the PCA9685 PWM servo/LED controller library.
 # This will move channel 0 from min to max position repeatedly.
-# Author: Tony DiCola
-# License: Public Domain
 from __future__ import division
 import time
 
@@ -21,13 +19,13 @@ pwm = Adafruit_PCA9685.PCA9685()
 
 # Configure min and max servo pulse lengths
 servo_min = 150  # Min pulse length out of 4096
-servo_max = 300  # Max pulse length out of 4096
+servo_max = 1000  # Max pulse length out of 4096
 
 
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
+print('Moving servo on channel 4, press Ctrl-C to quit...')
 while True:
     # Move servo on channel O between extremes.
     #360=clockwise, 0=counterclockwise
