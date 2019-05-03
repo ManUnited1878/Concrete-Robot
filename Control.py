771f24eb5020aa01113dc1c59310117fbb71c5fa
ManUnited1878 <ktf3@pdx.edu>
2019-05-02 17:51:39 -0700

    Changed forward/reverse control such that it activates only when key is pressed

--- Control.py
+++ Control.py
@@ -18,27 +18,42 @@ socketio = SocketIO(app)
 @socketio.on('arrows')
 def handle_message(message):
     print('received message: ' + str(message))
+    
+    #Forward Control
     if message==1:
         servo_min = 150  # Min pulse length out of 4096
         servo_max = 600  # Max pulse length out of 4096
 
+      # Set frequency to 60hz, good for servos.
+        pwm.set_pwm_freq(60)
+
+        print('Moving servos on channels 0-3, press Ctrl-C to quit...')
+        
+        # Move servo on channel O between extremes.
+        #360=clockwise, 0=counterclockwise
+        pwm.set_pwm(0, 0, servo_max) #Left Front Drive 
+        pwm.set_pwm(1, 360, servo_max) #Right Front Drive
+        pwm.set_pwm(2, 0, servo_max) #Left Rear Drive 
+        pwm.set_pwm(3, 360, servo_max) #Right Rear Drive
+
+    #Stop Forward Control      
+    if message==5: 
+        servo_min = 0  # Min pulse length out of 4096
+        servo_max = 600  # Max pulse length out of 4096
 
       # Set frequency to 60hz, good for servos.
         pwm.set_pwm_freq(60)
 
-        print('Moving servo on channels 0-3, press Ctrl-C to quit...')
-        while True:
-          # Move servo on channel O between extremes.
-          #360=clockwise, 0=counterclockwise
-          pwm.set_pwm(0, 0, servo_max) #Left Front Drive 
-          time.sleep(1)
-          pwm.set_pwm(1, 360, servo_max) #Right Front Drive
-          time.sleep(1)
-          pwm.set_pwm(2, 0, servo_max) #Left Rear Drive 
-          time.sleep(1)
-          pwm.set_pwm(3, 360, servo_max) #Right Rear Drive
-          time.sleep(1)
-   
+        print('Moving servos on channels 0-3, press Ctrl-C to quit...')
+        
+        # Move servo on channel O between extremes.
+        #360=clockwise, 0=counterclockwise
+        pwm.set_pwm(0, 0, servo_min) #Left Front Drive 
+        pwm.set_pwm(1, 0, servo_min) #Right Front Drive
+        pwm.set_pwm(2, 0, servo_min) #Left Rear Drive 
+        pwm.set_pwm(3, 0, servo_min) #Right Rear Drive
+
+    #Reverse Control
     if message==3:
         servo_min = 150  # Min pulse length out of 4096
         servo_max = 600  # Max pulse length out of 4096
@@ -48,18 +63,32 @@ def handle_message(message):
         pwm.set_pwm_freq(60)
 
         print('Moving servo on channels 0-3, press Ctrl-C to quit...')
-        while True:
-          # Move servo on channel O between extremes.
-          #360=clockwise, 0=counterclockwise
-          pwm.set_pwm(0, 360, servo_max) #Left Front Drive 
-          time.sleep(1)
-          pwm.set_pwm(1, 0, servo_max) #Right Front Drive
-          time.sleep(1)
-          pwm.set_pwm(2, 360, servo_max) #Left Rear Drive 
-          time.sleep(1)
-          pwm.set_pwm(3, 0, servo_max) #Right Rear Drive
-          time.sleep(1)
-  
+        
+        # Move servo on channel O between extremes.
+        #360=clockwise, 0=counterclockwise
+        pwm.set_pwm(0, 360, servo_max) #Left Front Drive 
+        pwm.set_pwm(1, 0, servo_max) #Right Front Drive
+        pwm.set_pwm(2, 360, servo_max) #Left Rear Drive 
+        pwm.set_pwm(3, 0, servo_max) #Right Rear Drive
+
+
+    #Stop Reverse Control      
+    if message==7: 
+        servo_min = 0  # Min pulse length out of 4096
+        servo_max = 600  # Max pulse length out of 4096
+
+      # Set frequency to 60hz, good for servos.
+        pwm.set_pwm_freq(60)
+
+        print('Moving servos on channels 0-3, press Ctrl-C to quit...')
+        
+        # Move servo on channel O between extremes.
+        #360=clockwise, 0=counterclockwise
+        pwm.set_pwm(0, 0, servo_min) #Left Front Drive 
+        pwm.set_pwm(1, 0, servo_min) #Right Front Drive
+        pwm.set_pwm(2, 0, servo_min) #Left Rear Drive 
+        pwm.set_pwm(3, 0, servo_min) #Right Rear Drive
+
     return render_template('index.html')
 @app.route("/")
 def root():
